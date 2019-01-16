ae141a6376bda9b7cbc256fbde6d9eeec321761d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-16 11:46:54 -0700

    added an index.php file

---
+++ epic/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Data Design Project Index</title>
+	</head>
+	<body>
+		<h1>Links</h1>
+			<ul>
+
+			</ul>
+	</body>
+</html>
