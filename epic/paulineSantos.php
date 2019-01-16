847f657ba80fcd8061ffd31e01fe4f59f4bd16ec
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-16 14:32:09 -0700

    Added a new .php file for the persona.

---
+++ epic/paulineSantos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>User Persona: Pauline Santos</title>
+	</head>
+	<body>
+		<h1>User Persona</h1>
+		<img src="paulineSantos.jpg" alt="Picture of user Pauline Santos">
+			<ul>
+				<li>Name: Pauline Santos</li>
+				<li>Age: 33</li>
+				<li>Gender Identity: Female</li>
+				<li>Location: Albuquerque, Nex Mexico</li>
+				<li>Occupation: Attorney</li>
+				<li>Relationship Status: Single</li>
+				<li>Likes: spending time with her children, meeting new people, and finding fun, local activities in which to participate</li>
+				<li>Technology: Desktop - 21.5-inch iMac; cell phone - Samsung Galaxy S8</li>
+				<li>Technological-comfort level: daily professional use (primarily Microsoft Office suite and internet) and basic personal web browsing and mobile application usage</li>
+				<li>Goals: Pauline would like to connect with local mothers for regular outings.</li>
+				<li>Frustrations:
+					<ul>
+						<li>Websites like Alibi.com don't list the kind of events she and her kids enjoy.</li>
+						<li>Other social networking sites' focus isn't meeting new people in her city.</li>
+					</ul>
+				</li>
+			</ul>
+	</body>
+</html>
