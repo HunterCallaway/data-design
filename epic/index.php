166b9546e6978c1c98b4878a861c48a369edfd5c
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-16 18:17:47 -0700

    Added an h1 tag to the index.php file.

--- epic/index.php
+++ epic/index.php
@@ -5,11 +5,12 @@
 		<title>Data Design Project Index</title>
 	</head>
 	<body>
-		<h1>Links</h1>
-			<ul>
-				<li><a href="pauline-santos.php">Persona</a></li>
-				<li><a href="use-case.php">Use Case</a></li>
-				<li><a href="conceptual-model.php">Conceptual Model</a></li>
-			</ul>
+		<h1>Data Design Project</h1>
+			<h2>Links</h2>
+				<ul>
+					<li><a href="pauline-santos.php">Persona</a></li>
+					<li><a href="use-case.php">Use Case</a></li>
+					<li><a href="conceptual-model.php">Conceptual Model</a></li>
+				</ul>
 	</body>
 </html>
