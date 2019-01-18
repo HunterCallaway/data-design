45a4eb024d87e20f6ed157923cc135888656e33d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-18 11:23:24 -0700

    Created two INSERT statements for the member table.

---
+++ mysql/oml.sql
@@ -0,0 +1,12 @@
+/*
+The following statement inserts data into the group table.
+ */
+
+ INSERT INTO `group` (groupId, groupHost, groupLocation, groupName) VALUES (UNHEX("5519ada6118145b5a10ed60e723caada"), "Amy Wu", "Albuquerque, New Mexico", "Desert Mommies Club");
+
+ /*
+ The following statements insert data into the member table.
+  */
+  INSERT INTO member (memberId, memberCreationDate, memberLocation, memberPassword, memberUserName) VALUES (UNHEX("7e9618f8d5e549468562a18c720ab159"), "2018-12-22 10:22:16", "Albuquerque, New Mexico", "hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha", "Pauline Marie Santos");
+  INSERT INTO member (memberID, memberCreationDate, memberLocation, memberPassword, memberUserName) VALUES (UNHEX("3b6db9e054684af2af3db45c48708263"), "2019-01-04 11:15:24", "Rio Rancho, New Mexico", "tetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetete", "Susan Howe");
+
