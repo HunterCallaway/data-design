fc0ec8fa9f984488f3074c996708ba67e6c9eb3a
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-18 11:46:43 -0700

    Updated a row in the member table and then deleted it.

--- mysql/oml.sql
+++ mysql/oml.sql
@@ -10,3 +10,12 @@ The following statement inserts data into the group table.
   INSERT INTO member (memberId, memberCreationDate, memberLocation, memberPassword, memberUserName) VALUES (UNHEX("7e9618f8d5e549468562a18c720ab159"), "2018-12-22 10:22:16", "Albuquerque, New Mexico", "hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha", "Pauline Marie Santos");
   INSERT INTO member (memberID, memberCreationDate, memberLocation, memberPassword, memberUserName) VALUES (UNHEX("3b6db9e054684af2af3db45c48708263"), "2019-01-04 11:15:24", "Rio Rancho, New Mexico", "tetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetete", "Susan Howe");
 
+/*
+The following statement updates a row in the member table.
+ */
+ UPDATE member SET memberLocation  = "Belen, New Mexico" WHERE memberID = UNHEX("3b6db9e054684af2af3db45c48708263");
+
+/*
+The following table deletes a row from the member table.
+ */
+ DELETE FROM member WHERE memberID = UNHEX("3b6db9e054684af2af3db45c48708263");
